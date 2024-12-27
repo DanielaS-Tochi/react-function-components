2955911e74d85e0aa0502f8f1bff693eee196677
DanielaS-Tochi <danielastochi@gmail.com>
2024-12-27 18:59:05 -0300

    Hooks

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,18 +1,42 @@
+//el useEffect lo quitamos del import porque comentamos las líneas 13 a 15 de useEffect
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Switch from '@mui/material/Switch';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+// import { FormControl } from '@mui/material';
 
 function FormSignUp() {
+    const [name, setName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [prom, setProm] = useState(true)
+    const [nov, setNov] = useState(false)
+
+
+    // useEffect(() => {
+    //     console.log("Nombre ha cambiado: ", name)
+    // }, [name])
+
     return (
-        <form>
+        <form
+            onSubmit={(e) => {
+                e.preventDefault()
+                console.log({ name, lastName, email, prom, nov })
+            }}
+        >
             <TextField
                 id="name"
                 label="Nombre"
                 variant="outlined"
                 fullWidth
                 margin="normal"
+                onChange={(e) =>
+                    // console.log(e.target.value)
+                    setName(e.target.value)
+                }
+                value={name}
             />
             <TextField
                 id="lastname"
@@ -20,6 +44,10 @@ function FormSignUp() {
                 variant="outlined"
                 fullWidth
                 margin="normal"
+                value={lastName}
+                onChange={(e) =>
+                    setLastName(e.target.value)
+                }
             />
             <TextField
                 id="email"
@@ -27,21 +55,28 @@ function FormSignUp() {
                 variant="outlined"
                 fullWidth
                 margin="normal"
+                value={email}
+                onChange={(e) =>
+                    setEmail(e.target.value)
+                }
             />
             <FormGroup>
                 <FormControlLabel
-                    control={<Switch defaultChecked />}
+                    control={<Switch checked={prom} onChange={(e) => setProm(e.target.checked)
+                    } />}
                     label="Promociones"
                 />
                 <FormControlLabel
-                    control={<Switch defaultChecked />}
+                    control={<Switch checked={nov} onChange={(e) => setNov(e.target.checked)
+
+                    } />}
                     label="Novedades"
                 />
 
             </FormGroup>
 
             {/* Por ejemplo, dentro de Button podemos agregar más clases: color="success" */}
-            <Button variant="contained" >Registrarse</Button>
+            <Button variant="contained" type="submit">Registrarse</Button>
         </form>
     )
 }
