e1a212d2b5d734fcb3eff57dd9c3ae1bc97d6bca
DanielaS-Tochi <danielastochi@gmail.com>
2024-12-26 21:15:02 -0300

    Formulario

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,10 +1,49 @@
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
 function FormSignUp() {
-    return <>
+    return (
         <form>
-            <input type="text" />
-            <button>Registrate</button>
+            <TextField
+                id="name"
+                label="Nombre"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                id="lastname"
+                label="Apellido"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                id="email"
+                label="Email"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+            />
+            <FormGroup>
+                <FormControlLabel
+                    control={<Switch defaultChecked />}
+                    label="Promociones"
+                />
+                <FormControlLabel
+                    control={<Switch defaultChecked />}
+                    label="Novedades"
+                />
+
+            </FormGroup>
+
+            {/* Por ejemplo, dentro de Button podemos agregar más clases: color="success" */}
+            <Button variant="contained" >Registrarse</Button>
         </form>
-    </>
+    )
 }
 
 export default FormSignUp
